204d140712017bafb948a73c2d34b7ec42e66351
Ronaldo Figueiredo <ronaldofigueiredo@MacBook-Air-de-Ronaldo-2.local>
2023-06-18 09:25:18 -0300

    test: adding a simple api route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,20 @@
+import { useEffect } from 'react'
 import { Header } from '../federated-components/Header'
 
+import axios from 'axios'
+
 export default function Home() {
+  useEffect(() => {
+    async function getData() {
+      const response = await axios.get('/api/hello')
+
+      console.log(response)
+    }
+
+    getData()
+  }, [])
+
+
   return (
     <Header />    
   )
